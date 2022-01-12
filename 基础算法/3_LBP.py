83eaad4693db22776bed69bbc855794d23c4c992
Xiaochaosui <chaosuixiao@gamil.com>
2022-01-12 10:32:56 +0800

    基础图像算法

---
+++ 基础算法/3_LBP.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*- 
+# @Time    : 2022/1/12 10:07
+# @Author  : xiaochaosui
+# @File    : 3_LBP.py
+# @Software: PyCharm
+# @Email   : chaosuixiao@gmail.com
+'''
+
+特征提取算法
+LBP（Local Binary Patterns，局部二值模式）是提取局部特征作为判别依据的。LBP方法显著的优点是对光照不敏感，但是依然没有解决姿态和表情的问题。不过相比于特征脸方法，LBP的识别率已经有了很大的提升。在[1]的文章里，有些人脸库的识别率已经达到了98%+。
+'''
